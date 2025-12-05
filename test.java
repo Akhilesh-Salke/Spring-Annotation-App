632571a235f4405679b9c8789a7a4d6b260eabca
Akhilesh-Salke <akhileshsalke7@gmail.com>
2025-12-05 09:29:57 +0530

    class added

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test{
+    public test() {
+        super();
+
+        System.out.println("This is Test class constructor...");
+    }
+    public void giveMsg(){
+        System.out.println("This is Test class in Annotation App ...");
+    }
+}
